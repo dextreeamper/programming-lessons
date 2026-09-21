6b4ab0ef3c8191c673f94e533136ad43a2781dd9
Dexter Amper <dextreeamper@gmail.com>
2026-09-21 19:29:29 +0800

    Created module 6 for array

---
+++ EDP/array-of-objects/index.js
@@ -0,0 +1,56 @@
+// https://jsonplaceholder.typicode.com/users
+// display only the 5 users
+
+const students = [
+  {
+    id: 1,
+    name: "Sei",
+    age: 20,
+    course: "BSIT",
+  },
+  {
+    id: 2,
+    name: "Anna",
+    age: 21,
+    course: "BSBA",
+  },
+  {
+    id: 3,
+    name: "Mota",
+    age: 23,
+    course: "BSBA",
+  },
+  {
+    id: 4,
+    name: "Bucay",
+    age: 20,
+    course: "BSBA",
+  },
+];
+
+// students.push({
+//   // add an object
+//   id: 4,
+//   name: "Bucay",
+//   age: 19,
+//   course: "BSIT",
+// });
+
+// students.pop(); // remove the last object
+// students.shift(); // remove the first object
+
+// students.unshift(); // add an object in the first element
+
+//display and filter the course
+// let filteredStudents = students.filter((student) => student.course === "BSBA");
+
+// console.log(filteredStudents);
+
+// for (const student of students) {
+//   console.log(student.name);
+//   console.log(student.age);
+//   console.log(student.course);
+//   console.log("==========");
+// }
+
+// console.log(students);
