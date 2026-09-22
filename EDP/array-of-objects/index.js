c8dedab59c71b2a126147dc226c04aa2e1f0da18
Dexter Amper <dextreeamper@gmail.com>
2026-09-22 19:18:16 +0800

    Array of Objects

--- EDP/array-of-objects/index.js
+++ EDP/array-of-objects/index.js
@@ -1,56 +1,71 @@
-// https://jsonplaceholder.typicode.com/users
-// display only the 5 users
-
 const students = [
   {
-    id: 1,
+    studentId: 1,
     name: "Sei",
     age: 20,
     course: "BSIT",
+    enrolled: true,
+    yearLevel: 3,
   },
   {
-    id: 2,
+    studentId: 2,
     name: "Anna",
+    age: 20,
+    course: "BSIT",
+    enrolled: false,
+    yearLevel: 2,
+  },
+  {
+    studentId: 3,
+    name: "Muta",
     age: 21,
     course: "BSBA",
+    enrolled: false,
+    yearLevel: 3,
   },
   {
-    id: 3,
-    name: "Mota",
-    age: 23,
-    course: "BSBA",
-  },
-  {
-    id: 4,
+    studentId: 4,
     name: "Bucay",
-    age: 20,
+    age: 21,
     course: "BSBA",
+    enrolled: true,
+    yearLevel: 2,
   },
 ];
 
-// students.push({
-//   // add an object
-//   id: 4,
-//   name: "Bucay",
-//   age: 19,
-//   course: "BSIT",
-// });
+// students.push; // add an object at the end of array
+students.unshift({
+  // add an object at the start of an element
+  studentId: 5,
+  name: "Neeh",
+  age: 24,
+  course: "BSBA",
+  enrolled: true,
+  yearLevel: 2,
+});
 
-// students.pop(); // remove the last object
-// students.shift(); // remove the first object
+// students.pop();
+// students.shift();
 
-// students.unshift(); // add an object in the first element
+let filteredStudents = students.filter((student) => student.course === "BSBA");
 
-//display and filter the course
-// let filteredStudents = students.filter((student) => student.course === "BSBA");
+console.log(filteredStudents);
 
-// console.log(filteredStudents);
+async function getComments() {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/posts/1/comments",
+  );
+  const data = await response.json();
 
-// for (const student of students) {
-//   console.log(student.name);
-//   console.log(student.age);
-//   console.log(student.course);
-//   console.log("==========");
-// }
+  let filteredComments = data.filter(
+    (comment) => comment.id === comment.postId,
+  );
 
-// console.log(students);
+  filteredComments.forEach((comment) => {
+    console.log(`Name: ${comment.name}`);
+    console.log(`Email: ${comment.email}`);
+    console.log(`Body: ${comment.body}`);
+  });
+}
+
+getComments();
